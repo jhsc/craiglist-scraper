cbc6bc67140d4621288e513326cde580d0110c78
jhsc <hi@jhsc.io>
2020-06-16 14:53:29 -0700

    use double tabs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import csv
 
 headers = ({
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
 })
 
 url = 'https://losangeles.craigslist.org/search/cpg?'
@@ -18,26 +18,26 @@ page_html = BeautifulSoup(r.text, 'html.parser')
 
 content = page_html.find_all('li', class_="result-row")
 if content != []:
-    for row in content:
-        info = row.find_all('a')[1]
-        title = info.text or ""
-        if any(word in title.lower() for word in keywords):
-            time = row.find_all('time')[0]
-            d = ({
-                'date': time.get('datetime'),
-                'title': info.text,
-                'href': info.get('href')
-            })
-            data.append(d)
+	for row in content:
+		info = row.find_all('a')[1]
+		title = info.text or ""
+		if any(word in title.lower() for word in keywords):
+			time = row.find_all('time')[0]
+			d = ({
+				'date': time.get('datetime'),
+				'title': info.text,
+				'href': info.get('href')
+			})
+			data.append(d)
 
 if len(data) > 0:
-    csv_file = datetime.now().ctime() + '.csv'
-    csv_columns = ['date', 'title', 'href']
-    try:
-        with open(csv_file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-            writer.writeheader()
-            for row in data:
-                writer.writerow(row)
-    except IOError:
-        print('I/O error')
+	csv_file = datetime.now().ctime() + '.csv'
+	csv_columns = ['date', 'title', 'href']
+	try:
+		with open(csv_file, 'w') as csvfile:
+			writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+			writer.writeheader()
+			for row in data:
+				writer.writerow(row)
+	except IOError:
+		print('I/O error')
